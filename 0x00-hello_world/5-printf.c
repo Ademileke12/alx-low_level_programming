8e413bf4728fa48cf842d3557eab68af220cc19d
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-02 15:32:49 -0700

    ;print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *  main - Entry point
  *
@@ -9,5 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
