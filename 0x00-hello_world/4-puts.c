a35fbdc17bca3f798d92d6db599ade809686897b
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-02 11:53:47 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multillingual puzzle/n");
+	return (0);
+}
