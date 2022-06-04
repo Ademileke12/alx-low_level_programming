73e5ce289c7192ebae65f93d5c4014f7ab93572b
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-03 19:31:36 -0700

    gh

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 0\n", n, n % 10);
 	}
 
 	return (0);
