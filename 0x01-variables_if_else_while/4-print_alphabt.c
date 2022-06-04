2cd7c58620ff651eb307386581565721b8db12ab
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-03 20:25:18 -0700

    nm

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,13 +3,13 @@
 /**
  * main - Prints the alphabet in lowercase, expect for q and e.
  *
- * Return :Always .0
+ * Return :Always 0.
  */
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'Z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
