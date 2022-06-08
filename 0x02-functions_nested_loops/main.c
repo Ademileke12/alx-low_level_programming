21236b854f60c4e970e2ea5566c6081b362a3a28
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-08 16:47:29 -0700

    new

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,14 +9,42 @@ int main(void)
 {
 	int r;
 
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
+	_putchar(',');
+
+	    _putchar(' ');
+
+	        _putchar(r + '0');
+
+		    _putchar('\n');
+
+		        r = print_sign(0);
+
+			    _putchar(',');
+
+			        _putchar(' ');
+
+				    _putchar(r + '0');
+
+				        _putchar('\n');
+
+					    r = print_sign(0xff);
+
+					        _putchar(',');
+
+						    _putchar(' ');
+
+						        _putchar(r + '0');
+
+							    _putchar('\n');
+
+							        r = print_sign(-1);
+
+								    _putchar(',');
+
+								        _putchar(' ');
+
+									    _putchar(r + '0');
+
+									        _putchar('\n');
 	return (0);
 }
