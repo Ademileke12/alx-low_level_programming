86d65dd66843e1f146bae1742151d6a904e71fa5
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-08 17:26:00 -0700

    new

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code.
@@ -8,19 +7,7 @@
  */
 int main(void)
 {
-	int r;
+	jack_bauer();
 
-
-
-	    print_last_digit(98);
-
-	        print_last_digit(0);
-
-		    r = print_last_digit(-1024);
-
-		        _putchar('0' + r);
-
-			    _putchar('\n');
-
-			        return (0);
+	    return (0);
 }
