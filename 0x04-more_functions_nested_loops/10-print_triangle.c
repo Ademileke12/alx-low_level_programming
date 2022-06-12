84b08e7a8677fda95887d49cf3c44c267e9b73aa
Ademileke12 <Ademilekeabudu@gmail.com>
2022-06-12 12:29:29 -0700

    new

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,16 @@ void print_triangle(int size)
 	{
 		for (; i < size; i++)
 		{
-			if (j < n)
-				_putchar(' ');
-			else
-				_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
 		}
-		n--;
-		_putchar('\n');
 	}
 	else
 		_putchar('\n');
