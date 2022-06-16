dda1b906786423c3c8a48b61d5107355a2af7cd2
Ademileke12 <ademilekeaudu@gmail.com>
2022-06-16 05:22:41 -0700

    new

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,7 +14,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here...
+	 * (p + 5) = 98;
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
