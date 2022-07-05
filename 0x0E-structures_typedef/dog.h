71395b8543721698c809f5901e004bcba268ac25
Ademileke12 <ademilekeaudu@gmail.com>
2022-07-05 06:26:42 -0700

    typeof

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 #endif
