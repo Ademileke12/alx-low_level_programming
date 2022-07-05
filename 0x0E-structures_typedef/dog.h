8b2d348dfcee5a9f49f4bb4ecbdadc58a9e5f926
Ademileke12 <ademilekeaudu@gmail.com>
2022-07-05 09:57:07 -0700

    new_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,6 +22,10 @@ void print_dog(struct dog *d);
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
+
+/**
+ * dog_t *new_dog - Typedef for dog_t
+ */
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
