e1e8d57ce63f4d750443886e831440f415965d5d
Ademileke12 <ademilekeaudu@gmail.com>
2022-07-05 05:05:03 -0700

    DOgs details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+* strcut dog - a struct with three elements
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*
+* Description: A struct that contains the details of a dog which stores
+* its name, age and owner.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
