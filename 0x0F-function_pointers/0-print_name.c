0b4d4d57195270343befc72dd0e682deb41bfcb6
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2022-07-06 23:23:50 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,9 +8,9 @@
  *
  * Return: Nothing
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *name))
 {
-	if (f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
