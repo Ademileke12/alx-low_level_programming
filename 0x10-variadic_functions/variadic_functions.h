73dc1a959af4800126401813e32b8a135ccb78bf
Ademileke12 <ademilekeaudu@gmail.com>
2022-07-07 18:52:19 -0700

    variadicfunc

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
